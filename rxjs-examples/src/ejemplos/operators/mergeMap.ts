5de1dba1c1739603ef5595aa78cd09b0f50a7622
Amaia Aretxalde <aaretxalde@irontec.com>
2022-05-20 09:44:16 +0200

    fix $

--- rxjs-examples/src/ejemplos/operators/mergeMap.ts
+++ rxjs-examples/src/ejemplos/operators/mergeMap.ts
@@ -1,7 +1,7 @@
 import { of, interval , mergeMap, map} from 'rxjs';
 
 const letters$ = of('a', 'b', 'c');
-const result$ = letters.pipe(
+const result$ = letters$.pipe(
   mergeMap(x => interval(1000).pipe(map(i => x + i)))
 );
 result$.subscribe(x => console.log(x));
