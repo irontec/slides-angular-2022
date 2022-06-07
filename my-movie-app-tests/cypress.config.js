fa3600048fc04f0aa8b0b1a8a5dbffecb1738ece
Amaia Aretxalde <aaretxalde@irontec.com>
2022-06-07 18:48:18 +0200

    wip

--- my-movie-app-tests/cypress.config.js
+++ my-movie-app-tests/cypress.config.js
@@ -1,12 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  /*   reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output.xml',
+    toConsole: true
+  }, */
+  reporter: "mochawesome",
+  reporterOptions: {
+    overwrite: false,
+    reportDir: "reports",
+    reportFilename: "Report",
+    code: "false",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    "baseUrl": "http://localhost:4200/",
-    "viewportHeight": 1080,
-    "viewportWidth": 1920
+    baseUrl: "http://localhost:4200/",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
   },
 });
